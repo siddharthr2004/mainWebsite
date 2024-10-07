8acd0b2d6f71944dfd196cbd587683d4eff9d8e7
Siddharths computer <sidbraj@Siddharthss-MacBook-Pro.local>
2024-10-07 18:04:30 -0500

    app.js functional 1st implementation

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
-const bodyParser = require('body-parser');
 const app = express();
 
-
-app.use(express.static(path.join(__dirname, 'public_html')));
+app.use(express.static('/home/siddaxsj/public_html'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public_html', 'mainPage.html'));
-  });
-
+    res.sendFile(path.join('/home/siddaxsj/public_html', 'mainPage.html'));
+});
 
 // Start the server
 const port = process.env.PORT || 3000;
